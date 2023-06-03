5ad01f80b461ae7e2701048ccf63bc7139311477
undefined <johann@objekt.stream>
2023-06-03 17:13:28 +0200

    ~ eslint

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
-import fsp from "fs/promises"
+import fsp from 'fs/promises'
 import { spawn } from 'child_process'
-import config from "../config.json"
+import config from '../config.json'
 
-type IResource = {
+interface IResource {
   name: string
   path: string
   watchCommand: string
@@ -14,10 +14,10 @@ getAllResources().then(resources => {
     console.log(`[hotreload] Watching ${resource.name} with command: ${resource.watchCommand}`)
 
     const watchProcess = spawn(resource.watchCommand, { shell: true, cwd: resource.path })
-    watchProcess.stdout.on("data", data => console.log(`[hotreload][${resource.name}][stdout] ${data.toString()}`))
-    watchProcess.stderr.on("data", data => console.log(`[hotreload][${resource.name}][stderr] ${data.toString()}`))
-    watchProcess.stdout.on("data", data => restartResource(resource.name, data))
-    watchProcess.on("close", code => console.log(`[hotreload][${resource.name}] Watch process exited with code ${code}`))
+    watchProcess.stdout.on('data', data => console.log(`[hotreload][${resource.name}][stdout] ${data.toString()}`))
+    watchProcess.stderr.on('data', data => console.log(`[hotreload][${resource.name}][stderr] ${data.toString()}`))
+    watchProcess.stdout.on('data', data => restartResource(resource.name, data))
+    watchProcess.on('close', code => console.log(`[hotreload][${resource.name}] Watch process exited with code ${code}`))
   }
 })
 
@@ -34,24 +34,22 @@ async function getAllResources () {
     const resourceName = GetResourceByFindIndex(i)
     const resourcePath = GetResourcePath(resourceName)
 
-
-    const manifest = await fsp.readFile(`${resourcePath}/fxmanifest.lua`, "utf-8").catch(() => null)
+    const manifest = await fsp.readFile(`${resourcePath}/fxmanifest.lua`, 'utf-8').catch(() => null)
 
     if (!manifest) continue
-    if (!manifest.includes("fxdk_watch_command")) continue
+    if (!manifest.includes('fxdk_watch_command')) continue
 
     const match = manifest.match(/fxdk_watch_command\s+'(.+?)'\s+{(.+?)}/)
     if (!match) continue
 
-    
     const [, command, args] = match
-    const commandArgs = args.split(",").map(arg => arg.trim().replace(/'/g, "")).join(" ")
+    const commandArgs = args.split(',').map(arg => arg.trim().replace(/'/g, '')).join(' ')
     const commandString = `${command} ${commandArgs}`
 
     resourceNames.push({
       name: resourceName,
       path: resourcePath,
-      watchCommand: commandString
+      watchCommand: commandString,
     })
   }
 
